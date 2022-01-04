7067efd727c14d02b5ea6e52e52d00ccde95638b
Val Hustle <stars@MacBook-Air.local>
2022-01-04 01:06:21 -0500

    commiting queries to that filter thrrough using SQL commands to make custom queries for: assignments per day, avg completion time, busy days, large cohorts, low avg completiont ime, total assignment submissions

---
+++ 3_queries_group-by/avg_completion_time.sql
@@ -0,0 +1,16 @@
+-- SELECT students.name as student, avg(assignment_submission.duration) as average_assignment_duration 
+-- FROM students
+
+-- JOIN assignment_submissions ON student_id = students.id
+
+-- WHERE students.end_date IS NULL
+
+-- GROUP BY student
+
+-- ORDER BY average_assignment_duration DESC;
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
