d8a28daff7d0a2da415e622ca642ce98f9815389
Val Hustle <stars@MacBook-Air.local>
2022-01-06 20:44:21 -0500

    updated students and teahcers to protect/prevent form SQL injection using parameterized queries, basically breaking down the query into 2 parts, first part is the querystring that is inputted and the second part is the array of values that is unsafe and inputted by the user which could be malicious, but the query only takes in certain parameteres that it is expecting

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const id = process.argv[2];
-
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${id}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2]
+const limit = process.argv[3] || 5;
+//all potentally malicious code is in array of values
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,24 @@ const pool = new Pool({
 });
 
 
-const cohortname = process.argv[2];
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+//all potentally malicious code is in array of values
+const values = [`${cohortName}`];
 
-pool.query(`
+
+const queryString= `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
+
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
